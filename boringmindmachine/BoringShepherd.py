3a3aa6465cc7f2c6e9b0dbc34500cfd1a6db7f40
Charles Reid <charlesreid1@gmail.com>
2018-05-19 18:28:24 -0700

    updating boring shepherd class. mostly clean.

--- boringmindmachine/BoringShepherd.py
+++ boringmindmachine/BoringShepherd.py
@@ -1,31 +1,41 @@
-import twitter
-import os, re, time, glob
-import simplejson as json
+from .BoringSheep import BoringSheep
+import glob
 
-from multiprocessing import Pool
-from multiprocessing.dummy import Pool as ThreadPool 
+"""
+BoringShepherd class 
 
-from .Sheep import *
-from .Lumberjack import *
 
-its_alive = r'''
- _______ _______ __ _______      _______ _____   _______ ___ ___ _______ __ __ __
-|_     _|_     _|  |     __|    |   _   |     |_|_     _|   |   |    ___|  |  |  |
- _|   |_  |   |  |_|__     |    |       |       |_|   |_|   |   |    ___|__|__|__|
-|_______| |___|    |_______|    |___|___|_______|_______|\_____/|_______|__|__|__|
-'''
+The Shepherd class spins up the flock of Sheep 
+and lets them roam free.
 
-class Shepherd(object):
+The BoringShepherd class leaves the details of creating Sheep
+to the user, but still defines some useful methods. 
+
+The constructor calls a method to setup the flock.
+The method to setup the flock loops over each key.
+For each key, it:
+    - validates the key
+    - creates a Sheep from the key
+
+There are also two methods to perform actions with the flock:
+one for serial and one for parallel.
+"""
+
+
+class BoringShepherd(object):
     """
-    Spin up the flock of Sheep and let them roam free
+
+    The Shepherd should behave the same for each API,
+    since all APIs require keys and all keys will go
+    in a JSON file. (Even if it is an empty one.)
     """
     def __init__(self, 
                  json_keys_dir, 
-                 flock_name,
-                 sheep_class=Sheep, 
+                 name,
+                 sheep_class=BoringSheep, 
                  **kwargs):
         """
-        Create a Shepherd.
+        This constructor will create a Shepherd.
 
             json_keys_dir:      Directory where Sheep API keys are located
 
@@ -34,134 +44,73 @@ class Shepherd(object):
             sheep_class:        Type of Sheep
 
             kwargs:             Logging parameters passed directly to Lumberjack logger
-
-        For example, a flock of Queneau Sheep is created like this:
-
-            sh = rmm.Shepherd(json_keys_dir = 'keys/',
-                              flock_name = 'Queneau Flock',
-                              sheep_class = rmm.QueneauSheep)
-
-        To pass a custom log file name to lumberjack:
-
-            sh = rmm.Shepherd(json_eys_dir = 'keys/',
-                              flock_name = 'Plain Old Flock of Sheep',
-                              log_file = '/path/to/my/log')
         """
-
-        # save flock name
-        self.name = flock_name
-
-        # contains each of our sheep
-        self.all_sheep = []
-
-        # contains json files with Twitter acct keys
-        # (one for each sheep)
-        self.all_json = []
-
-        # Create a Lumberjack instance to configure the logs.
-        # This sets the log configuration for a logger named
-        # 'rainbowmindmachine'
-        # 
-        # Rather saving lumberjack, we just say
-        # logger = logging.getLogger('rainbowmindmachine')
-        # logger.info('sjasdljflasdkjflksj')
-        # 
+        # Create a lumberjack to set up the logs
         lumberjack = Lumberjack(**kwargs)
-        # 
-        # Set it and forget it, 
-        # we don't need lumberjack anymore
+        # We won't need the lumberjack anymore
 
-        self._setup_keys(json_keys_dir)
-        self._setup_sheep(sheep_class)
+        # Shepherds have to keep watch over their flock
+        self.flock = []
+        self.create_flock(json_keys_dir)
 
-        # should probably set self.params
-        # before calling setup keys/sheep
-        self.params = kwargs
-
-
-    def _key_is_valid(self, json_key_file):
+    def create_flock(self, json_keys_dir):
         """
-        Given a JSON file with a bot key,
-        check if it is a valid key.
+        Create a flock of Sheep from a given directory of keys.
+
+        Note that separating code out into 
+        a method like this makes it easier to
+        customize the sheep-key initialization
+        behavior of the Keymaker.
         """
-        required_keys = ['consumer_token',
-                        'consumer_token_secret',
-                        'oauth_token',
-                        'oauth_token_secret',
-                        'user_id']
 
-        bot_key = {}
-        with open(json_key_file,'r') as f:
-            bot_key = json.load(f)
+        # For each key:
+        # - validate key
+        # - set up sheep with key
+        # These methods need to be defined by 
+        # the derived class.
 
-        for rk in required_keys:
-            if rk not in bot_key.keys():
-                return False
+        for json_file in glob.glob(os.path.join(self.json_keys_dir,'*')):
+            bot_key = {}
+            with open(json_file,'r') as f:
+                bot_key = json.load(f)
 
-        return True
+            self._validate_key(bot_key)
+            self._create_sheep(bot_key)
 
 
-    def _setup_keys(self, json_keys_dir):
+    def _validate_key(self, key):
+        """virtual method"""
+        err = "ERROR: _validate_key() method must be defined in derived class."
+        raise Exception(err)
+
+
+    def _create_sheep(self, key):
+        """virtual method"""
+        err = "ERROR: _create_sheep() method must be defined in derived class."
+        raise Exception(err)
+
+
+    def perform_serial_action(self, action, **kwargs):
         """
-        For each json bot key file in the keys directory,
-        validate the key, then add it to the list.
-
-        This assumes the key has already been made
-        with the Keymaker.
+        Perform an action with each bot in serial.
+        Good for actions like updating profile.
         """
-        logger = logging.getLogger('rainbowmindmachine')
-        logger.info("Bot Flock Shepherd: Validating keys")
-        json_key_files = glob.glob(os.path.join(json_keys_dir, "*.json"))
-        for json_key_file in json_key_files:
-            if self._key_is_valid(json_key_file):
-                logger.info("Key %s: VALID"%(json_key_file))
-                self.all_json.append(json_key_file)
-            else:
-                logger.info("Key %s: INVALID"%(json_key_file))
-
-
-    def _setup_sheep(self,sheep_class):
-        """
-        For each validated json bot key file,
-        initialize a Sheep object with that bot key.
-        """
-        logger = logging.getLogger('rainbowmindmachine')
-
-        MySheepClass = sheep_class
-        logger.info("Bot Flock Shepherd: Creating flock %s"%(self.name))
-
-        for jsonf in self.all_json:
-            logger.info("Bot Flock Shepherd: Making Sheep for file %s"%(jsonf))
-            mysheep = MySheepClass(jsonf)
-            self.all_sheep.append(mysheep)
-
-        logger.info("Bot Flock Shepherd: Successfully created flock %s"%(self.name))
-        logger.info(its_alive)
-
-
-    def perform_action(self,action,params={}):
-        """
-        Perform an action with each bot iteratively.
-
-        Good for things like updating profile information.
-        """
-        if len(self.all_sheep) > 0:
-            for sheep in self.all_sheep:
-                sheep.perform_action(action,params)
+        if len(self.flock)>0:
+            for sheep in self.flock:
+                sheep.perform_action(action, params)
         else:
-            raise Exception("Error: The shepherd has no sheep!")
+            err = "ERROR: The shepherd has no sheep!"
+            raise Exception(err)
 
 
-    def perform_pool_action(self,action,params={}):
+    def perform_parallel_action(self, action, **kwargs):
         """
-        Create a thread pool to perform an action in parallel.
-
+        Perform an action with each bot in parallel.
         Good for tweeting, searching, and continuous tasks.
         """
-
         def do_it(sheep):
-            sheep.perform_action(action,params)
+            sheep.perform_action(action, **kwargs)
 
-        pool = ThreadPool(len(self.all_sheep))
-        results = pool.map(do_it,self.all_sheep)
+        pool = ThreadPool(len(self.flock))
+        results = pool.map(do_it,self.flock)
 
