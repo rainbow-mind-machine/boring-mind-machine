15808cf895620ea043454ea0be64135bf9349282
Charles Reid <charlesreid1@gmail.com>
2018-05-20 17:04:45 -0700

    account for move

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
-from boringmindmachine import __version__
+from bmm import __version__
 
 setup(  name='boringmindmachine',
         version=__version__,
         description='Boring base classes for awesome bot flocks.',
-        url='https://pages.charlesreid1.com/rainbow-mind-machine',
+        url='https://pages.charlesreid1.com/boring-mind-machine',
         author='charlesreid1',
         author_email='charles@charlesreid1.com',
         test_suite='nose.collector',
         tests_require=['nose'],
         license='MIT',
-        packages=['bmm'],
+        packages=['boringmindmachine'],
         install_requires=[],
         zip_safe=False)
 
