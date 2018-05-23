e0b94aefd8b862af31147a5d28f384eea9d4492a
Charles Reid <charlesreid1@gmail.com>
2018-05-22 20:32:57 -0700

    adding updated makefile ruleset

--- rules.mk
+++ rules.mk
@@ -8,10 +8,41 @@ SHELL := /bin/bash
 
 default: 
 
-########################
-# documentation groundwork
 
-initialize_ghpages: fix_remotes
+##########
+# utils
+
+fix_remotes: 
+	git remote set-url origin $(BMM_GH)
+	git remote set-url cmr $(BMM_CMR)
+
+submodule_init:
+	git submodule update --init
+
+
+##############
+# init_docs
+
+init_docs: fix_remotes init_site init_mkdocs_material
+
+init_site:
+	rm -rf site
+	git clone -b gh-pages $(BMM_GH) site \
+	cd site && git remote add cmr $(BMM_CMR)
+
+init_mkdocs_material:
+	wget https://tinyurl.com/sample-mkdocs-yml -O mkdocs.yml
+	git submodule add $(MKM_CMR) \
+		&& git add mkdocs-material mkdocs.yml .gitmodules \
+		&& git commit mkdocs-material mkdocs.yml .gitmodules -m 'Initializing mkdocs-material submodule' \
+		&& git push origin \
+	mkdir docs && cp README.md docs/index.md
+
+
+#############
+# init_gh
+
+init_gh: fix_remotes
 	rm -r site && git clone $(BMM_GH) site
 	set -x \
 		&& cd site/ \
@@ -25,52 +56,17 @@ initialize_ghpages: fix_remotes
 		&& git push cmr gh-pages \
 		&& set +x
 
-initialize_docs: fix_remotes site mkdocs_material
 
-mkdocs_material:
-	git submodule add $(MKM_CMR) \
-		&& git add mkdocs-material .gitmodules \
-		&& git commit mkdocs-material .gitmodules -m 'Initializing mkdocs-material submodule' \
-		&& git push origin \
-		&& wget https://git.charlesreid1.com/docker/pod-charlesreid1/raw/branch/master/mkdocs.yml \
-	mkdir docs && cp README.md docs/index.md
+###############
+# deploy_docs
 
-site:
-	if [ ! -d site ]; then \
-		git clone -b gh-pages $(BMM_GH) site \
-	fi 
-	cd site && git remote add cmr $(BMM_CMR)
-
-fix_remotes: 
-	git remote set-url origin $(BMM_GH)
-	git remote set-url cmr $(BMM_CMR)
-
-cmr_remote:
-	git remote add cmr $(BMM_CMR)
-
-submodule_init:
-	git submodule update --init
-
-
-########################
-# deploy documentation
-
-deploy: site
-	rm -rf site/*
+deploy_docs: init_site
 	mkdocs build
-	cd site; git add -A .; git commit -a -m 'Updating gh-pages site'; git push origin gh-pages; git push cmr gh-pages
-
-
-#######################
-# src init
-
-initialize_src:
-	mkdir $(SHORT)
-	touch $(SHORT)/__init__.py
-	git add $(SHORT) \
-		&& git commit $(SHORT) -m 'init commit of src' \
-		&& git push origin master \
-		&& git push cmr master
+	cd site \
+		&& git add -A . \
+		&& git commit -a -m 'updating gh-pages site' \
+		&& git push origin gh-pages \
+		&& git push cmr gh-pages
 
 
 
