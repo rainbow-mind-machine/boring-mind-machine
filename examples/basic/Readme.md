c774df8c6044f612e4aea5c692d5e6f8197f844f
Charles Reid <charlesreid1@gmail.com>
2018-06-13 00:26:49 -0700

    adding a basic example

---
+++ examples/basic/Readme.md
@@ -0,0 +1,11 @@
+# basic example
+
+A simple example of creating an OAuth keymaker.
+
+To run this:
+
+```
+python basic_auth.py
+```
+
+This loads keys in `fake_apikeys.json`.
---
+++ examples/basic/basic_auth.py
@@ -0,0 +1,18 @@
+import boringmindmachine as bmm
+import os, subprocess, json
+import tempfile
+
+token_var = 'token'
+secret_var = 'secret'
+keys_json = "fake_apikeys.json"
+keypath = os.path.join(os.getcwd(),keys_json)
+
+bk = bmm.BoringOAuthKeymaker(token=token_var,
+                             secret=secret_var)
+bk.set_apikeys_file(keypath)
+
+if(bk.credentials[token_var] == 'AAAAA' and
+    bk.credentials[secret_var] == 'BBBBB'):
+    print("Successfully created a BoringOAuthKeymaker")
+
+
