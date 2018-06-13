e0cacfe5c27dcd0b6bd0fc07ca26d607a9ff28a2
Charles Reid <charlesreid1@gmail.com>
2018-06-12 23:25:39 -0700

    add import of github/google/twitter keymakers

--- boringmindmachine/__init__.py
+++ boringmindmachine/__init__.py
@@ -1,5 +1,8 @@
 from .BoringLumberjack import *
 from .BoringKeymaker import *
+from .GithubKeymaker import *
+from .GoogleKeymaker import *
+from .TwitterKeymaker import *
 from .BoringShepherd import *
 from .BoringSheep import *
 
