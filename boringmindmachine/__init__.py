700629dab04a50ddb9211a8e9409d4f3cb993623
your name here <charlesreid1@gmail.com>
2018-07-18 22:58:03 -0700

    move platform-specific keymakers to platform-specific libraries

--- boringmindmachine/__init__.py
+++ boringmindmachine/__init__.py
@@ -1,7 +1,7 @@
 from .BoringKeymaker import *
-from .GithubKeymaker import *
-from .GoogleKeymaker import *
-from .TwitterKeymaker import *
+#from .GithubKeymaker import *
+#from .GoogleKeymaker import *
+#from .TwitterKeymaker import *
 from .BoringShepherd import *
 from .BoringSheep import *
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = "7"
+version = "13"
 
 setup(  name='boringmindmachine',
         version=version,
@@ -15,11 +15,7 @@ setup(  name='boringmindmachine',
         install_requires=[
             'oauth2>=1.5',
             'simplejson>=3.13',
-            'python-twitter>=3.4.1',
             'oauth2client>=3.0.0',
-            'PyGithub>=1.39',
-            'google-api-python-client>=1.7.3',
-            'pyasn1-modules>=0.2.1',
             'apiclient',
             'httplib2>=0.10.3',
             'requests>=1.0.0'],
