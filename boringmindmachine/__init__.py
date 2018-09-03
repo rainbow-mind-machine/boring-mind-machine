754978a261db63607cff50e92fb29a152ffb4855
Charles Reid <charlesreid1@gmail.com>
2018-09-03 11:04:09 -0700

    fix error

--- boringmindmachine/__init__.py
+++ boringmindmachine/__init__.py
@@ -3,10 +3,19 @@ from .BoringShepherd import *
 from .BoringSheep import *
 
 import logging
+import os
+from datetime import datetime
+
+LOGDIR = '/tmp/mind-machine/'
+
+unique = datetime.now().strftime("%Y%M%d_%H%m%S")
+log_filename = "mindmachine_%s.log"%(unique)
+log_file = os.path.join(LOGDIR,log_filename)
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     datefmt='%m-%d %H:%M',
                     filename=log_file,
                     filemode='w')
 
-logging.info('BoringMindMachine __init__: Logging has been set up')
+logging.info('BoringMindMachine: package init: Logging has been set up')
